06a5b566e91db3f85ff660a397517f08e68fb8ad
CraigMcCorrisken <craigmccorrisken@gmail.com>
2019-09-11 19:14:10 +0100

    0.0.2 - Adding a window
    Signed-off-by: CraigMcCorrisken <craigmccorrisken@gmail.com>

--- Serious Games Project/Serious Games Project/main.cpp
+++ Serious Games Project/Serious Games Project/main.cpp
@@ -1,10 +1,45 @@
 #pragma once
 #include <SDL.h>
+#include <GL/glew.h>
 #include <iostream>
+#include "Window.h"
 #include "Mojito.h"
 #include "Game.h"
 
+//#if _DEBUG
+//#pragma comment(linker, "/subsystem:\"console\" /entry:\"WinMainCRTStartup\"")
+//#endif
+
+
 Game game = Game();
+
+
+bool handleSDLEvent(SDL_Event const& sdlEvent)
+{
+	if (sdlEvent.type == SDL_QUIT)
+		return false;
+	if (sdlEvent.type == SDL_KEYDOWN)
+	{
+		// Can extend this to handle a wider range of keys
+		switch (sdlEvent.key.keysym.sym)
+		{
+		case SDLK_ESCAPE:
+			return false;
+		default:
+			break;
+		}
+	}
+	return true;
+}
+
+void exitFatalError(char* message)
+{
+	cout << message << " " << endl;
+	cout << SDL_GetError();
+	SDL_Quit();
+	exit(1);
+}
+
 void init()
 {
 	game.init();
@@ -23,6 +58,21 @@ void draw()
 
 int main(int argc, char* argv[])
 {
+	SDL_GLContext glContext; // OpenGL context handle
+	Window* hWindow = new Window(800, 600, "Serious Games Project"); // window handle
+
+	hWindow->setupRC(glContext);
+	SDL_Renderer* renderTarget = nullptr;
+	renderTarget = SDL_CreateRenderer(hWindow->getWindow(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err) { // glewInit failed, something is seriously wrong
+		std::cout << "glewInit failed, aborting." << endl;
+		exit(1);
+	}
+	cout << glGetString(GL_VERSION) << endl;
+
 	init();
 
 	SDL_Event sdlEvent;	// variable to detect SDL events
@@ -37,5 +87,8 @@ int main(int argc, char* argv[])
 		update(sdlEvent);
 		draw();
 	}
+
+	SDL_DestroyWindow(hWindow->getWindow());
+	SDL_Quit();
 	return 0;
 }
