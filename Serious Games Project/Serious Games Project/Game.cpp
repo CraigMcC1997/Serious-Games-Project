4f1e1d74bf445cb682d0eabbb0ea5f91c4f5e4d5
craig mccorrisken <craigmccorrisken@gmail.com>
2019-09-17 20:07:33 +0100

    0.0.4 - Implemeted reading from file, improvements needed for use in project
    Signed-off-by: craig mccorrisken <craigmccorrisken@gmail.com>

--- Serious Games Project/Serious Games Project/Game.cpp
+++ Serious Games Project/Serious Games Project/Game.cpp
@@ -5,6 +5,24 @@ void Game::init()
 {
 	cout << mojito->getName() << endl;
 	mojito->getIngredients();
+
+	if (myfile.is_open())
+	{
+		while (!myfile.eof())
+		{
+			myfile >> name;
+			myfile >> var1;
+			myfile >> var2;
+			myfile >> var3;
+			myfile >> var4;
+			cout << name << " " << var1 << " " << var2 << " " << var3 << " " << var4 << endl;
+		}
+
+		myfile.close();
+
+	}
+
+	else cout << "Unable to open file";
 }
 
 void Game::update()
--- Serious Games Project/Serious Games Project/Game.h
+++ Serious Games Project/Serious Games Project/Game.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Mojito.h"
 #include <iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -8,6 +12,15 @@ class Game
 {
 private:
 	Mojito* mojito = new Mojito();
+
+	string name;
+	string var1;
+	string var2;
+	string var3;
+	string var4;
+
+	ifstream myfile = ifstream("../Cocktails/Cocktails.txt");
+
 public:
 	void init();
 	void update();
